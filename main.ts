6ca402a4df1687ca4bbb0d696dafc3f702494dd4
Aiman2315 <naqviaiman44@gmail.com>
2024-05-22 00:31:53 +0500

    Push to github

---
+++ main.ts
@@ -0,0 +1,33 @@
+#! usr/bin/env node
+import inquirer from "inquirer"
+
+let todos = [];
+let condition = true;
+
+while(condition)
+    {
+        let addTask = await inquirer.prompt(
+        [
+           {
+            name: "todo",
+            message: "What you want to add in your Todos?",
+            type: "input",
+           },
+
+           {
+            name: "addmore",
+            message: "Do you want to add more?",
+            type: "confirm",
+            default: "false",
+           },
+
+        ]
+
+    );
+    //  console.log(addTask.todo);
+    
+        todos.push(addTask.todo);
+        condition = addTask.addmore
+        console.log(todos);    
+       
+    }
